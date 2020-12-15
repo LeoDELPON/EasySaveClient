1dd8e56f053d84afe101ebd7a2f9896e25a38b54
LeoDELPON <leo.delponviacesi@gmail.com>
2020-12-15 23:01:27 +0100

    cast DTO attributes

--- EasySaveClient/Factory/DataServerFactory.cs
+++ EasySaveClient/Factory/DataServerFactory.cs
@@ -11,17 +11,17 @@ namespace EasySaveClient.Factory
         public override DTODataServer CreateDtoDataServer(Dictionary<WorkProperties, object> propertiesServer)
         {
             DTODataServer dataServer = new DTODataServer();
-            dataServer.Date = propertiesServer[WorkProperties.Date].ToString();
+            dataServer.Date = (DateTime)propertiesServer[WorkProperties.Date];
             dataServer.Name = propertiesServer[WorkProperties.Name].ToString();
             dataServer.Source = propertiesServer[WorkProperties.Source].ToString();
             dataServer.Target = propertiesServer[WorkProperties.Target].ToString();
-            dataServer.Size = propertiesServer[WorkProperties.Size].ToString();
-            dataServer.Duration = propertiesServer[WorkProperties.Duration].ToString();
-            dataServer.EncryptDuration = propertiesServer[WorkProperties.EncryptDuration].ToString();
+            dataServer.Size = (int)propertiesServer[WorkProperties.Size];
+            dataServer.Duration = (DateTime)propertiesServer[WorkProperties.Duration];
+            dataServer.EncryptDuration = (int)propertiesServer[WorkProperties.EncryptDuration];
             dataServer.State = propertiesServer[WorkProperties.State].ToString();
-            dataServer.EligibleFiles = propertiesServer[WorkProperties.EligibleFiles].ToString();
+            dataServer.EligibleFiles = (int)propertiesServer[WorkProperties.EligibleFiles];
             dataServer.Progress = (double)propertiesServer[WorkProperties.Progress];
-            dataServer.RemainingSize = propertiesServer[WorkProperties.RemainingSize].ToString();
+            dataServer.RemainingSize = (int)propertiesServer[WorkProperties.RemainingSize];
             return dataServer;
         }
     }
