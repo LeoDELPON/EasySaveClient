0c6e9f76b8e1978f8edc5b3b9b3f1e61d9ec02c1
Kylianmgt <kylian.migot@viacesi.fr>
2020-12-16 09:32:18 +0100

    issou

--- EasySaveClient/MainWindow.xaml.cs
+++ EasySaveClient/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace EasySaveClient
         private void InitializeSocket()
         {
             _client = new ClientSocket();
-            _client.Connect("192.168.226.1", 9999);
+            _client.Connect("192.168.43.138", 9999);
             
         }
 
